e2e3a735aaaee47787c33dea3f884fa0d9603c72
LenouarMiloud <lenouarmiloud@gmail.com>
2026-08-19 16:03:05 -0800

    modify the GameModeScreen File

--- app/src/main/java/com/fsociety/tictactoe/ui/screens/GameModeScreen.kt
+++ app/src/main/java/com/fsociety/tictactoe/ui/screens/GameModeScreen.kt
@@ -1,9 +1,26 @@
 package com.fsociety.tictactoe.ui.screens
 
+import android.graphics.Color
+import android.widget.Space
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 enum class Difficulty{
     EASY,
@@ -25,5 +42,38 @@ fun GameModeScreen(
     var selectedDifficulty by remember{
         mutableStateOf(Difficulty.MEDIUM)
     }
+
+    var selectedFirstPlayer by remember {
+        mutableStateOf(FirstPlayer.HUMAN)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF101018))
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "VS PHONE",
+            color = Color.White,
+            fontSize = 36.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+
+        Spacer(Modifier.height(35.dp))
+    }
     
 }
+
+
+
+
+
+
+
+
+
+
+
