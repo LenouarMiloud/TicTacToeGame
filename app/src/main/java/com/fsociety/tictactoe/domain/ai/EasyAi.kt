052b3837e7e53e1594d450df562b46466dd35bea
LenouarMiloud <lenouarmiloud@gmail.com>
2026-08-29 13:24:50 -0800

    modify the EasyAi file

--- app/src/main/java/com/fsociety/tictactoe/domain/ai/EasyAi.kt
+++ app/src/main/java/com/fsociety/tictactoe/domain/ai/EasyAi.kt
@@ -6,7 +6,7 @@ object EasyAi {
     fun getMove(board: List<String>): Int?{
         val availableMoves = GameLogic.getAvailableMoves(board)
         if(availableMoves.isEmpty()){
-            
+            return null
         }
         return availableMoves.random()
     }
